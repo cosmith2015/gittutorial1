4edfa3274ff82ff8820921e029e77a908488ea6e
SNR User <snruser@unl.edu>
2018-10-19 11:31:02 -0500

    Created a new column detailing if the fish is big or small, dependent upon 200.

---
+++ Challenge_Fishes.R
@@ -0,0 +1,14 @@
+
+## library tidy verse ## load data from a data folderfolder
+## create a new column in data called length_cat length >200 its big otherwise small 
+#3 commit w/ message
+## push 
+
+library(tidyverse)
+library(dplyr)
+
+fish_data %>%  mutate (length_cat=case_when(length >= 200 ~ "Big", length<200 ~ "Small" ))->Fishes 
+
+head(Fishes) 
+
+
